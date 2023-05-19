77712d899074bed20200895ba8587cc43eeccf63
IThundxr <contact@ithundxr.dev>
2023-05-19 16:14:20 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,51 @@
+const PostalMime = require("postal-mime");
+
+async function streamToArrayBuffer(stream, streamSize) {
+	let result = new Uint8Array(streamSize);
+	let bytesRead = 0;
+	const reader = stream.getReader();
+	while (true) {
+		const { done, value } = await reader.read();
+		if (done) {
+			break;
+		}
+		result.set(value, bytesRead);
+		bytesRead += value.length;
+	}
+	return result;
+}
+
+export default {
+	async email(message, env, ctx) {
+		const rawEmail = await streamToArrayBuffer(message.raw, message.rawSize);
+		const parser = new PostalMime.default();
+		const parsedEmail = await parser.parse(rawEmail);
+
+		await fetch(env.DISCORD_WEBHOOK_URL, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				embeds: [{
+						title: "DMARC Report",
+						description: `Got a DMARC report from ${message.from} \nsubject: ${message.headers.get("subject")} \ntext: ${parsedEmail.text}`,
+					}]
+				}),
+		});
+		console.log(parsedEmail.attachments)
+		parsedEmail.attachments.forEach(async att => {
+			console.log(att)
+
+			const form = new FormData();
+				form.append("file1", att);
+			await fetch(env.DISCORD_WEBHOOK_URL, {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/form-data",
+				},
+				body: form
+			});
+		});
+	},
+};
